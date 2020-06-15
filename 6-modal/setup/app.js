e8581063e7b311ab6268200e9fea7103556da5cc
Elshadai Tegegn <elshadaikassutegegn@gmail.com>
2020-06-15 18:18:21 +0300

    Functionality for modal page complete

--- 6-modal/setup/app.js
+++ 6-modal/setup/app.js
@@ -2,3 +2,14 @@
 // listen for click events on modal-btn and close-btn
 // when user clicks modal-btn add .open-modal to modal-overlay
 // when user clicks close-btn remove .open-modal from modal-overlay
+const openModal = document.querySelector('.modal-btn');
+const closeModal = document.querySelector('.close-btn');
+
+const modalOverlay = document.querySelector('.modal-overlay');
+
+openModal.addEventListener('click', function(){
+    modalOverlay.classList.toggle('open-modal')
+});
+closeModal.addEventListener('click', function(){
+    modalOverlay.classList.remove('open-modal')
+})
