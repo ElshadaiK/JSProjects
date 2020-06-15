c60fcbefdf840053cf22e31698f9dab196e49d98
Elshadai Tegegn <elshadaikassutegegn@gmail.com>
2020-06-15 19:31:18 +0300

    Fuctionality finished for questions page with two ways

--- 7-questions/setup/app.js
+++ 7-questions/setup/app.js
@@ -1,2 +1,20 @@
 //using selectors inside the element
+const questions = document.querySelectorAll('.question');
+questions.forEach(function(question){
+    const btn = question.querySelector('.question-btn');
+    btn.addEventListener('click', function(){
+        questions.forEach(function(question){
+            question.classList.remove('show-text')
+        })
+        question.classList.toggle('show-text');
+    })
+})
+// ORRRRRRR
 // traversing the dom
+// const btns = document.querySelectorAll('.question-btn');
+// btns.forEach(function(btn){
+//     btn.addEventListener('click', function(event){
+//         event.currentTarget.parentElement.parentElement.classList.toggle('show-text');
+        
+//     })
+// });
