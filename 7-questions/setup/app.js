4d599cdd7c1056ae01b7fff152361f80248e7569
Elshadai Tegegn <elshadaikassutegegn@gmail.com>
2020-06-15 19:34:44 +0300

    Fuctionality made a little better by excluding one loop

--- 7-questions/setup/app.js
+++ 7-questions/setup/app.js
@@ -3,8 +3,10 @@ const questions = document.querySelectorAll('.question');
 questions.forEach(function(question){
     const btn = question.querySelector('.question-btn');
     btn.addEventListener('click', function(){
-        questions.forEach(function(question){
-            question.classList.remove('show-text')
+        questions.forEach(function(item){
+            if(item !== question){
+            item.classList.remove('show-text');
+        }
         })
         question.classList.toggle('show-text');
     })
