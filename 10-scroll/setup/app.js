42bb205d9cb96dd7cd45d6994a82c69888bcc517
Elshadai Tegegn <elshadaikassutegegn@gmail.com>
2020-06-16 13:38:16 +0300

    Fixed navbar and back to top features added

--- 10-scroll/setup/app.js
+++ 10-scroll/setup/app.js
@@ -24,6 +24,19 @@ navToggle.addEventListener('click', function(){
     }
 })
 // ********** fixed navbar ************
-
+const navbar = document.getElementById('nav');
+const topLink = document.querySelector('.top-link')
+window.addEventListener('scroll', function(){
+    const scrollHeight = window.pageYOffset;
+    const navHeight = navbar.getBoundingClientRect().height;
+    if(scrollHeight > navHeight){
+        navbar.classList.add('fixed-nav');
+        topLink.classList.add('show-link');
+    }
+    else{
+        navbar.classList.remove('fixed-nav');
+        topLink.classList.remove('show-link');
+    }
+})
 // ********** smooth scroll ************
 // select links
