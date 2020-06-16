32e1215393bd8ff6c3971adbdaf58f85d9738eb5
Elshadai Tegegn <elshadaikassutegegn@gmail.com>
2020-06-16 17:31:45 +0300

    Date display finished

--- 12-counter/setup/app.js
+++ 12-counter/setup/app.js
@@ -21,3 +21,18 @@ const weekdays = [
   "Friday",
   "Saturday",
 ];
+const giveaway = document.querySelector('.giveaway');
+const deadline = document.querySelector('.deadline');
+const items = document.querySelectorAll('.deadline-format h4');
+let futureDate = new Date(2020, 11, 9, 11, 30, 0);
+// console.log(futureDate)
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+let month = futureDate.getMonth();
+month = months[month];
+const date = futureDate.getDate();
+let day = futureDate.getDay();
+day = weekdays[day]
+
+giveaway.textContent = `Giveaway ends on ${day}, ${date} ${month} ${year} at ${hours}:${minutes}` 
