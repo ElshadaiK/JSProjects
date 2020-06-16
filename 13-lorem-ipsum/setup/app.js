27b33acebf8d2cf96451aca09eeecf2380d89f3a
Elshadai Tegegn <elshadaikassutegegn@gmail.com>
2020-06-16 19:39:31 +0300

    Functionality of Lorem Ipsum project finished to give random paragraphs

--- 13-lorem-ipsum/setup/app.js
+++ 13-lorem-ipsum/setup/app.js
@@ -11,3 +11,35 @@ I just told you! You've killed me! Fry! Quit doing the right thing, you jerk! Mi
   `Man braid celiac synth freegan readymade, pitchfork fam salvia waistcoat lomo bitters gentrify four loko. Pitchfork semiotics post-ironic vegan. Tofu meditation microdosing hashtag semiotics venmo. Flexitarian vape tilde taiyaki. Prism poutine farm-to-table, messenger bag vegan taxidermy tattooed sartorial squid jean shorts fixie selvage trust fund vape.`,
   `Rutters Plate Fleet boom chandler Brethren of the Coast handsomely lookout marooned brigantine knave. Buccaneer gangway jack rum loot spyglass line Jack Tar fore gaff. Gaff topmast scuttle ballast swab draught measured fer yer chains dance the hempen jig Chain Shot yardarm.`,
 ];
+
+const form = document.querySelector('.lorem-form');
+const amount = document.getElementById('amount');
+const result = document.querySelector('.lorem-text');
+
+form.addEventListener('submit', function(event){
+  event.preventDefault();
+  const value = parseInt(amount.value);
+  if(isNaN(value) || value < 0 || value >9){
+    randomValue = Math.floor(Math.random() * text.length);
+    result.innerHTML = `<p class='result'>${text[randomValue]}</p>`
+  }
+  else{
+    let tempText = text.slice(0);
+    let finalText = []
+    for (let index = 0; index < value; index++) {
+      randomValue = Math.floor(Math.random() * tempText.length);
+      const element = tempText[randomValue];
+      finalText.push(element);
+      tempText.pop(element);
+    }
+    result.innerHTML = "";
+    finalText.forEach(function(paragraphs){
+      let paragraph = document.createElement('p');
+      paragraph.classList.add('result');
+      paragraph.textContent = paragraphs;
+      result.appendChild(paragraph);
+    })
+    
+  }
+  
+})
